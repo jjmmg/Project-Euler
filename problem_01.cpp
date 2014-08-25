3f9d5ce351f38bed374092503d1948f2074d7021
Juanjo Martin <jjmmg@yahoo.com>
2014-08-25 17:45:20 +0200

    Problem 1

---
+++ problem_01.cpp
@@ -0,0 +1,22 @@
+// Multiples of 3 and 5
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+// Let's use STL! Just for fun, since there are simpler solutions.
+
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <iterator>
+
+int main()
+{
+    std::vector<int> v(999);
+    std::iota(v.begin(), v.end(), 1);
+
+    int result = std::accumulate(v.begin(), v.end(), 0, [](int sum, int elem)
+        {   return (!(elem % 3) || !(elem % 5)) ? sum + elem : sum;   });
+ 
+    std::cout << "Sum: " << result << '\n';
+}
